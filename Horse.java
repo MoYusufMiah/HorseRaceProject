3438af49e94d536d92da56a3567442b0bafd8d77
Yusuf <ec24525@qmul.ac.uk>
2025-04-24 18:50:57 +0100

    added functions to horse class

--- Horse.java
+++ Horse.java
@@ -8,7 +8,11 @@
 public class Horse
 {
     //Fields of class Horse
-    
+    private char symbol;
+    private String name;
+    private int distance;
+    private boolean fallen;
+    private double confidence;
     
       
     //Constructor of class Horse
@@ -25,52 +29,57 @@ public class Horse
     //Other methods of class Horse
     public void fall()
     {
-        
+        fallen=true;
     }
     
     public double getConfidence()
     {
-        
+        return confidence;
     }
     
     public int getDistanceTravelled()
     {
-        
+        return distance;
     }
     
     public String getName()
     {
-        
+        return name;
     }
     
     public char getSymbol()
     {
-        
+        return symbol;
     }
     
     public void goBackToStart()
     {
-        
+        distance=0;
     }
     
     public boolean hasFallen()
     {
-        
+        if (fallen==true){
+		return true;
+	}
+	else {
+		return false;
+	}
     }
 
     public void moveForward()
     {
-        
+        distance=distance+1;
     }
 
     public void setConfidence(double newConfidence)
     {
-        
+        confidence= newConfidence;
     }
     
     public void setSymbol(char newSymbol)
     {
-        
+        symbol = newSymbol;
     }
     
 }
